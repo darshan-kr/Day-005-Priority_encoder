ef4b0ce83c30e03d98b32271df37ea79595833a8
darshan-kr <69472429+darshan-kr@users.noreply.github.com>
2023-07-23 11:48:21 +0530

    Update Priority_encoder.v

--- Priority_encoder.v
+++ Priority_encoder.v
@@ -8,15 +8,15 @@ module Priority_encoder(
     else
       begin
         casex(in)
-      8'bxxxxxxx1:y = 3'd0;
-      8'bxxxxxx10:y = 3'd1;
-      8'bxxxxx100:y = 3'd2;
-      8'bxxxx1000:y = 3'd3;
-      8'bxxx10000:y = 3'd4;
-      8'bxx100000:y = 3'd5;
-      8'bx1000000:y = 3'd6;
-      8'b10000000:y = 3'd7;
-      default:y = 3'd0;
+      8'b1xxxxxxx:y = 3'd7;
+      8'b01xxxxxx:y = 3'd6;
+      8'b001xxxxx:y = 3'd5;
+      8'b0001xxxx:y = 3'd4;
+      8'b00001xxx:y = 3'd3;
+      8'b000001xx:y = 3'd2;
+      8'b0000001x:y = 3'd1;
+      8'b00000001:y = 3'd0;
+      default:y = 3'd7;
     endcase
     
       end
